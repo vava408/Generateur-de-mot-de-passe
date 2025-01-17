97289db9248eede99bf7fa0c3901921a778fb936
vava408 <valentinlemaitre06@gmail.com>
2025-01-17 08:42:11 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,29 @@
+import iut.algo.*;
+
 public class main
 {
-	public static void main
-	{
+	public static void main()
+		{
 		int longueur;
 		int nombreDeMajuscule;
 		int nombreDeCaratereSpeciale;
+
+		System.out.println("Entrer une longueur de mot de passe");
+		longueur = Clavier.lire_int();
+		if(longueur > 0)
+		{
+			System.out.println("Entrer le nombre de majuscule");
+			nombreDeMajuscule = Clavier.lire_int();
+			if (nombreDeMajuscule > 0 && nombreDeMajuscule <= longueur -1)
+			{
+				System.out.println("Entrer le nombre de caratere special");
+				nombreDeCaratereSpeciale = Clavier.lire_int();
+				
+
+			}
+
+
+		}
 		
 	}
 }
